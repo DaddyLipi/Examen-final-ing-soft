bf2dfd04f7b0fcda1d671160a702a563d4714e56
Jose Manuel Sanchez <99229719+DaddyLipi@users.noreply.github.com>
2022-06-07 11:20:48 -0400

    Refactoring, lista numeros mejorada, sin if

--- src/Numeros.test.js
+++ src/Numeros.test.js
@@ -14,17 +14,6 @@ describe("Iterador de numeros", () => {
   {
     let listaNumeros=["cero","uno","dos"]
     let numerolit;
-      if(numero==0)
-      {
-        numerolit=listaNumeros[0];
-      }
-      else if(numero==1)
-      {
-        numerolit=listaNumeros[1];
-      }
-      else if(numero==2)
-      {
-        numerolit=listaNumeros[2];
-      }
-      return numerolit;
+    numerolit=listaNumeros[numero];
+    return numerolit;
   }
