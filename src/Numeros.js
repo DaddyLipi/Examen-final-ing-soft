897fc916a59b3da787e67561464cc6b9ba5c9145
Jose Manuel Sanchez <99229719+DaddyLipi@users.noreply.github.com>
2022-06-07 11:09:07 -0400

    TDD 1, ingreso 0, devuleve cero

---
+++ src/Numeros.js
@@ -0,0 +1,9 @@
+describe("Iterador de numeros", () => {
+    it("Da el numero 0", () => {
+      expect(NumeroLiteral(0)).toEqual("cero");
+    });
+  });
+  function NumeroLiteral(numero)
+  {
+      return "cero";
+  }
